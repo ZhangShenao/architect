3625e8e169178f1bf2b406ec5999774b462394a3
ZhangShenao <15201440436@163.com>
2019-02-23 20:45:04 +0800

    RocketMQ Producer & Consumer API

--- architect/rocketmq/src/main/java/william/rmq/consumer/ClusteringPushConsumer.java
+++ architect/rocketmq/src/main/java/william/rmq/consumer/ClusteringPushConsumer.java
@@ -1,17 +1,17 @@
-package william.rmq.quickstart;
+package william.rmq.consumer;
 
 import org.apache.rocketmq.client.consumer.DefaultMQPushConsumer;
 import org.apache.rocketmq.common.consumer.ConsumeFromWhere;
 import org.apache.rocketmq.common.protocol.heartbeat.MessageModel;
 import william.rmq.common.RocketMQConstants;
-import william.rmq.retry.RetryListener;
+import william.rmq.listener.SimpleListener;
 
 /**
  * @Auther: ZhangShenao
  * @Date: 2018/11/4 09:54
- * @Description:消息消费者,默认采用Push模式
+ * @Description:集群模式的消息消费者,采用Push模式
  */
-public class Consumer {
+public class ClusteringPushConsumer {
     public static void main(String[] args) {
         try {
             //创建DefaultMQPushConsumer
@@ -26,11 +26,11 @@ public class Consumer {
             //设置消费者启动时,初始的消费位置(从Queue头/尾开始消费)。Consumer启动后,offset会随着对消息的消费逐渐前进
             consumer.setConsumeFromWhere(ConsumeFromWhere.CONSUME_FROM_FIRST_OFFSET);
 
-            //订阅Topic,并且可以根据Tag对消息进行过滤
-            consumer.subscribe(RocketMQConstants.TOPIC_NAME, "*");
+            //订阅Topic,并且可以根据Tag对消息进行过滤,这个过滤是在Consumer端实现的
+            consumer.subscribe(RocketMQConstants.TOPIC_NAME, "TagA || TagB");
 
             //注册消息监听器
-            consumer.registerMessageListener(new RetryListener());
+            consumer.registerMessageListener(new SimpleListener());
 
             //启动Consumer
             consumer.start();
