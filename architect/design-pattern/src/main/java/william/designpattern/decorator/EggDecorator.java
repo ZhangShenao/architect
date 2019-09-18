9b2445e1eaae4a23810cb21441fb0ce51b1c8a43
ZhangShenao <15201440436@163.com>
2019-09-18 18:14:20 +0800

    Decorator Pattern

---
+++ architect/design-pattern/src/main/java/william/designpattern/decorator/EggDecorator.java
@@ -0,0 +1,24 @@
+package william.designpattern.decorator;
+
+/**
+ * @Author zhangshenao
+ * @Date 2019-09-18
+ * @Description 加鸡蛋的装饰器
+ */
+public class EggDecorator extends PancakeDecoretor {
+    public EggDecorator(Pancake pancake) {
+        super(pancake);
+    }
+
+    @Override
+    public String material() {
+        return pancake.material()
+                + " + "
+                + "一个鸡蛋";
+    }
+
+    @Override
+    public int price() {
+        return pancake.price() + 1;
+    }
+}
---
+++ architect/design-pattern/src/main/java/william/designpattern/decorator/TestDecorator.java
@@ -0,0 +1,22 @@
+package william.designpattern.decorator;
+
+/**
+ * @Author zhangshenao
+ * @Date 2019-09-18
+ * @Description 装饰器模式:在不改变原有对象的基础之上,将功能附加到对象上。
+ * 装饰器模式提供了比继承更有弹性的替代方案,可以动态地扩展原有对象的功能
+ * 装饰器模式属于结构型模式。
+ * <p>
+ * 适用场景:动态扩展一个类的功能
+ */
+public class TestDecorator {
+    public static void main(String[] args) {
+        Pancake pancake = new PurePancake();
+        pancake = new EggDecorator(pancake);
+        pancake = new EggDecorator(pancake);
+        pancake = new SausageDecorator(pancake);
+        System.err.println("原料: " + pancake.material() + " 价格: " + pancake.price());
+
+
+    }
+}
