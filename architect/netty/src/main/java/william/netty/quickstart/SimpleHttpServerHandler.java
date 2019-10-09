c8d263c5e68f4e21712243fea16a0dfcc84678ad
ZhangShenao <15201440436@163.com>
2019-10-09 20:57:22 +0800

    Netty HttpServer

--- architect/netty/src/main/java/william/netty/quickstart/SimpleHttpServerHandler.java
+++ architect/netty/src/main/java/william/netty/quickstart/SimpleHttpServerHandler.java
@@ -1,38 +1,40 @@
 package william.netty.quickstart;
 
+import static io.netty.handler.codec.http.HttpHeaderNames.CONTENT_LENGTH;
+import static io.netty.handler.codec.http.HttpHeaderNames.CONTENT_TYPE;
+import static io.netty.handler.codec.http.HttpResponseStatus.OK;
+import static io.netty.handler.codec.http.HttpVersion.HTTP_1_1;
+import static io.netty.util.CharsetUtil.UTF_8;
+
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
-import io.netty.handler.codec.http.*;
-import io.netty.util.CharsetUtil;
+import io.netty.handler.codec.http.DefaultFullHttpResponse;
+import io.netty.handler.codec.http.FullHttpResponse;
+import io.netty.handler.codec.http.HttpRequest;
 
 /**
  * @Auther: ZhangShenao
  * @Date: 2019/1/16 13:17
- * @Description:
+ * @Description: Http请求处理器
  */
-public class SimpleHttpServerHandler extends SimpleChannelInboundHandler<HttpObject> {
+public class SimpleHttpServerHandler extends SimpleChannelInboundHandler<HttpRequest> {
     @Override
-    protected void channelRead0(ChannelHandlerContext ctx, HttpObject msg) {
-        if (!(msg instanceof HttpRequest)){
-            return;
-        }
-
-        //获取HTTP请求的相关信息
-        HttpRequest request = (HttpRequest)msg;
+    protected void channelRead0(ChannelHandlerContext ctx, HttpRequest request) {
         System.err.println("请求方式: " + request.method().name());
         System.err.println("请求URI: " + request.uri());
         System.err.println("客户端地址: " + ctx.channel().remoteAddress());
-        //响应内容
-        ByteBuf content = Unpooled.copiedBuffer("Hello World", CharsetUtil.UTF_8);
+        //通过ByteBuf构造响应内容
+        ByteBuf content = Unpooled.copiedBuffer("Hello World", UTF_8);
 
         //使用FullHttpResponse构造HTTP Response对象
-        FullHttpResponse response = new DefaultFullHttpResponse(HttpVersion.HTTP_1_1,
-                HttpResponseStatus.OK, content);
+        FullHttpResponse response = new DefaultFullHttpResponse(HTTP_1_1, OK, content);
+
+        //设置响应头信息
         response.headers()
-                .set(HttpHeaderNames.CONTENT_TYPE, "text/plain")
-                .set(HttpHeaderNames.CONTENT_LENGTH, content.readableBytes());
+                .set(CONTENT_TYPE, "text/plain")
+                .set(CONTENT_LENGTH, content.readableBytes());
 
         //写入响应数据
         ctx.writeAndFlush(response);
