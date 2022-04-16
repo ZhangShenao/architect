1fe34f39fd383dbd5ff005ff9120b6e9bd62e2d8
Shenao Zhang <shenao.zhang@shopee.com>
2022-04-16 23:39:01 +0800

    Bubble Sort

--- architect/interview/src/main/java/william/interview/algo/sort/QuickSort.java
+++ architect/interview/src/main/java/william/interview/algo/sort/QuickSort.java
@@ -17,6 +17,29 @@ public class QuickSort {
         quickSort(arr, 0, arr.length - 1);
     }
 
+    public static void sort(Integer[] arr) {
+        if (arr == null || arr.length <= 0) {   //边界条件
+            return;
+        }
+
+        quickSort(arr, 0, arr.length - 1);
+    }
+
+    //对数组arr的[start,end]区间进行快速排序
+    private static void quickSort(Integer[] arr, int start, int end) {
+        //递归终止条件
+        if (start >= end) {
+            return;
+        }
+
+        //选取基准值,将数组一分为二
+        int pivot = partition(arr, start, end);
+
+        //对左、右两部分分别进行快速排序
+        quickSort(arr, start, pivot - 1);
+        quickSort(arr, pivot + 1, end);
+    }
+
     //对数组arr的[start,end]区间进行快速排序
     private static void quickSort(int[] arr, int start, int end) {
         //递归终止条件
@@ -34,6 +57,39 @@ public class QuickSort {
 
     //对数组arr的[start,end]区间内进行partition操作
     private static int partition(int[] arr, int start, int end) {
+        //固定选择第一个元素作为基准值
+        int pivot = arr[start];
+
+        //双路指针
+        int i = start + 1;
+        int j = end;
+
+        while (true) {
+            while (i <= end && arr[i] < pivot) {    //找到左边第一个比基准值大的元素
+                ++i;
+            }
+            while (j >= start && arr[j] > pivot) {  //找到右边第一个比基准值小的元素
+                --j;
+            }
+            if (i >= j) {
+                break;
+            }
+
+            //交换元素
+            Swap.swap(arr, i, j);
+
+            //进行下一轮
+            i++;
+            j--;
+        }
+
+        //最终j代表最后一个小于基准值的位置,i代表第一个大于基准值的位置。j就是基准值的位置,交换元素
+        Swap.swap(arr, start, j);
+        return j;
+    }
+
+    //对数组arr的[start,end]区间内进行partition操作
+    private static int partition(Integer[] arr, int start, int end) {
         //固定选择第一个元素作为基准值
         int pivot = arr[start];
 
--- architect/interview/src/main/java/william/interview/algo/utils/Swap.java
+++ architect/interview/src/main/java/william/interview/algo/utils/Swap.java
@@ -7,4 +7,11 @@ public class Swap {
         arr[i] = arr[j];
         arr[j] = tmp;
     }
+
+    //交换数组i,j下标处的元素
+    public static void swap(Integer[] arr, int i, int j) {
+        int tmp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = tmp;
+    }
 }
