8f06c1980908a1663caf43baa0aa1c3513ef32ac
Shenao Zhang <shenao.zhang@shopee.com>
2022-04-19 11:01:31 +0800

    stack

---
+++ architect/interview/src/main/java/william/interview/algo/stack/有效的括号20.java
@@ -0,0 +1,50 @@
+package william.interview.algo.stack;
+
+import java.util.Stack;
+
+/**
+ * https://leetcode-cn.com/problems/valid-parentheses/
+ */
+public class 有效的括号20 {
+    public boolean isValid(String s) {
+        //边界条件
+        if (s == null || s.length() <= 0) {
+            return true;
+        }
+
+        //使用栈暂存左括号
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            //如果是左括号,则直接入栈
+            if (('(' == c) || ('[' == c) || ('{' == c)) {
+                stack.push(c);
+                continue;
+            }
+
+            //如果是右括号,则需要匹配栈顶的左括号
+            if (stack.isEmpty()) {
+                return false;
+            }
+
+            char b = stack.pop();
+            if (!match(c, b)) {
+                return false;
+            }
+        }
+
+        //最后返回栈是否为空
+        return stack.isEmpty();
+    }
+
+    private boolean match(char right, char left) {
+        if (')' == right) {
+            return ('(' == left);
+        }
+        if (']' == right) {
+            return ('[' == left);
+        }
+        return ('{' == left);
+    }
+}
