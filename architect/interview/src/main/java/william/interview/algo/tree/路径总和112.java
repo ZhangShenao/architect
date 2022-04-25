7b86048f4dad69cf9039cae5fd3bc63ade22b405
Shenao Zhang <shenao.zhang@shopee.com>
2022-04-25 14:39:49 +0800

    tree algos

---
+++ architect/interview/src/main/java/william/interview/algo/tree/路径总和112.java
@@ -0,0 +1,42 @@
+package william.interview.algo.tree;
+
+/**
+ * https://leetcode-cn.com/problems/path-sum/description/
+ */
+public class 路径总和112 {
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+        //边界条件
+        if (root == null) {
+            return false;
+        }
+
+        //递归判断
+        return hasPathSum(root, 0, targetSum);
+    }
+
+
+    private boolean hasPathSum(TreeNode root, int current, int targetSum) {
+        //递归终止条件
+        if (root == null) {
+            return (current == targetSum);
+        }
+
+        //更新当前路径和
+        current += root.val;
+        //遍历到叶子节点,判断当前路径和
+        if (root.left == null && root.right == null) {
+            return (current == targetSum);
+        }
+
+        //递归判断左、右子树(注意如果子树为空则不认为是一条路径)
+        if (root.left == null) {
+            return hasPathSum(root.right, current, targetSum);
+        }
+        if (root.right == null) {
+            return hasPathSum(root.left, current, targetSum);
+        }
+
+        //左、右子树均不为空的情况
+        return hasPathSum(root.left, current, targetSum) || hasPathSum(root.right, current, targetSum);
+    }
+}
---
+++ architect/interview/src/main/java/william/interview/algo/tree/验证二叉搜索树98.java
@@ -0,0 +1,41 @@
+package william.interview.algo.tree;
+
+/**
+ * https://leetcode-cn.com/problems/validate-binary-search-tree/description/
+ */
+public class 验证二叉搜索树98 {
+    //全局控制条件
+    private boolean valid = true;
+
+    public boolean isValidBST(TreeNode root) {
+        //边界条件
+        if (root == null) {  //空树也是二叉搜索树
+            return true;
+        }
+
+        //从根节点开始递归遍历
+        preOrder(root, Long.MIN_VALUE, Long.MAX_VALUE);
+
+        return valid;
+    }
+
+    //对二叉树进行一次前序遍历,一次判断每个节点是否在指定范围内(影子二叉树)
+    private void preOrder(TreeNode root, long left, long right) {
+        if (!valid) {   //递归终止条件
+            return;
+        }
+        if (root == null) {  //遍历到叶子节点
+            return;
+        }
+
+        //不满足BST定义,直接终止
+        if (root.val <= left || root.val >= right) {
+            valid = false;
+            return;
+        }
+
+        //递归判断左、右子树
+        preOrder(root.left, left, root.val);
+        preOrder(root.right, root.val, right);
+    }
+}
