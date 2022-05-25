2121ea8a6e6b8ca86fd4a919b7567b28fc65b2ab
ZhangShenao <15201440436@163.com>
2022-05-25 10:49:57 +0800

    Add Two Numbers from List

--- architect/leetcode/src/main/java/william/leetcode/list/删除链表的倒数第N个结点_19.java
+++ architect/leetcode/src/main/java/william/leetcode/list/删除链表的倒数第N个结点_19.java
@@ -8,22 +8,29 @@ package william.leetcode.list;
  * https://leetcode-cn.com/problems/remove-nth-node-from-end-of-list/description/
  */
 public class 删除链表的倒数第N个结点_19 {
+    //采用双指针法:前、后指针
+    //前指针向前进n步
+    //然后前、后指针同时前进
+    //当前指针移动到链表尾部时,后指针指向倒数第n+1个节点,直接删除倒数第n个节点即可
+    //时间复杂度O(N) 仅需遍历一次链表
+    //空间复杂度O(1)
     public ListNode removeNthFromEnd(ListNode head, int n) {
         //边界条件
-        if (head == null || n <= 0) {
+        if (head == null || n < 1) {
             return head;
         }
 
-        //使用front、behind两个指针,front始终比behind超前n个节点,这样当front遍历到链表尾部时,front刚好指向倒数第n个节点
+        //采用前、后双指针
+        ListNode dummy = new ListNode(0, head);  //借助哑头
         ListNode front = head;
-        ListNode dummy = new ListNode(0, head);  //借助哑结点
         ListNode behind = dummy;
 
+        //前front指针先前进n步
         for (int i = 0; i < n; i++) {
             front = front.next;
         }
 
-        //将front遍历到链表末尾,最终behind指向倒数第n个节点的前驱节点
+        //然后front、behind指针同时前进,当front移动到链表末尾时,behind就指向倒数第n+1个节点
         while (front != null) {
             front = front.next;
             behind = behind.next;
